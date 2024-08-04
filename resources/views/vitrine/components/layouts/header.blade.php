f117e385ecfcd1db5c3a8554b8d732fa2c1c1862
Abdoul-le-dev <abdoulledev@gmail.com>
2024-08-04 19:51:19 +0200

    Abdoul

--- resources/views/vitrine/components/layouts/header.blade.php
+++ resources/views/vitrine/components/layouts/header.blade.php
@@ -1,166 +1,140 @@
 <header class="section page-header rd-navbar-dark">
     <!-- RD Navbar-->
     <div class="rd-navbar-wrap">
-        <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed"
-            data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fixed"
-            data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static"
-            data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static"
-            data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="166px" data-xl-stick-up-offset="166px"
-            data-xxl-stick-up-offset="166px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
-            <div class="rd-navbar-panel">
-                <!-- RD Navbar Toggle-->
-                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-main"><span></span></button>
-                <!-- RD Navbar Panel-->
-                <div class="rd-navbar-panel-inner container">
-                    <div class="rd-navbar-collapse rd-navbar-panel-item rd-navbar-panel-item-left">
-                        <!-- Owl Carousel-->
-                        <div class="owl-carousel-navbar owl-carousel-inline-outer">
-                            <div class="owl-inline-nav">
-                                <button class="owl-arrow owl-arrow-prev"></button>
-                                <button class="owl-arrow owl-arrow-next"></button>
-                            </div>
-                            <div class="owl-carousel-inline-wrap">
-                                <div class="owl-carousel owl-carousel-inline" data-items="1" data-dots="false"
-                                    data-nav="true" data-autoplay="true" data-autoplay-speed="3200"
-                                    data-stage-padding="0" data-loop="true" data-margin="10" data-mouse-drag="false"
-                                    data-touch-drag="false" data-nav-custom=".owl-carousel-navbar">
-                                    <!-- Post Inline-->
-                                    <article class="post-inline">
-                                        <time class="post-inline-time" datetime="2024">April 15, 2024</time>
-                                        <p class="post-inline-title">Sportland vs Dream Team</p>
-                                    </article>
-                                    <!-- Post Inline-->
-                                    <article class="post-inline">
-                                        <time class="post-inline-time" datetime="2024">April 15, 2024</time>
-                                        <p class="post-inline-title">Sportland vs Real Madrid</p>
-                                    </article>
-                                    <!-- Post Inline-->
-                                    <article class="post-inline">
-                                        <time class="post-inline-time" datetime="2024">April 15, 2024</time>
-                                        <p class="post-inline-title">Sportland vs Barcelona</p>
-                                    </article>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="rd-navbar-panel-item rd-navbar-panel-item-right">
-                        <ul class="list-inline list-inline-bordered">
-                            <li>
-                                <!-- Select 2-->
-                                <select class="select" name="lang" data-container-class="select-inline"
-                                    data-dropdown-class="select-inline-dropdown">
-                                    <option selected="">fr</option>
-
-
-                                </select>
-                            </li>
-                            <li>
-                                <div class="cart-inline-toggled-outer">
-                                    <!-- Link Cart-->
-                                    <button class="link link-cart cart-inline-toggle"
-                                        data-rd-navbar-toggle="#cart-inline"><span
-                                            class="link-cart-icon fa-phone-square"></span></button>
-                                    <!-- Cart Inline-->
-                                    <article class="cart-inline cart-inline-toggled" id="cart-inline">
-                                        <div class="cart-inline-inner">
-
-
-                                            <div class="cart-inline-footer flex flex-row">
-                                                <a class="button button-md button-default-outline">+22968732237</a>
-                                                <a class="button button-md button-primary"
-                                                    href="mailto::jaydenfootballacademy@gmail.com">:jaydenfootballacademy@gmail.com</a>
-                                            </div>
-                                        </div>
-                                    </article>
-                                </div>
-                            </li>
-                            <li><a class="link link-icon link-icon-left link-classic"
-                                    href="{{route('login.view')}}"><span
-                                        class="icon fl-bigmug-line-login12"></span><span class="link-icon-text">Votre
-                                        Compte</span></a></li>
-                        </ul>
-                    </div>
-                    <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1"
-                        data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+      <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="166px" data-xl-stick-up-offset="166px" data-xxl-stick-up-offset="166px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+        <div class="rd-navbar-panel">
+          <!-- RD Navbar Toggle-->
+          <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-main"><span></span></button>
+          <!-- RD Navbar Panel-->
+          <div class="rd-navbar-panel-inner container">
+            <div class="rd-navbar-collapse rd-navbar-panel-item rd-navbar-panel-item-left">
+              <!-- Owl Carousel-->
+              <div class="owl-carousel-navbar owl-carousel-inline-outer">
+                <div class="owl-inline-nav">
+                  <button class="owl-arrow owl-arrow-prev"></button>
+                  <button class="owl-arrow owl-arrow-next"></button>
                 </div>
+                <div class="owl-carousel-inline-wrap">
+                  <div class="owl-carousel owl-carousel-inline" data-items="1" data-dots="false" data-nav="true" data-autoplay="true" data-autoplay-speed="3200" data-stage-padding="0" data-loop="true" data-margin="10" data-mouse-drag="false" data-touch-drag="false" data-nav-custom=".owl-carousel-navbar">
+                    <!-- Post Inline-->
+                    <article class="post-inline">
+                      <time class="post-inline-time" datetime="2024">April 15, 2024</time>
+                      <p class="post-inline-title">Sportland vs Dream Team</p>
+                    </article>
+                    <!-- Post Inline-->
+                    <article class="post-inline">
+                      <time class="post-inline-time" datetime="2024">April 15, 2024</time>
+                      <p class="post-inline-title">Sportland vs Real Madrid</p>
+                    </article>
+                    <!-- Post Inline-->
+                    <article class="post-inline">
+                      <time class="post-inline-time" datetime="2024">April 15, 2024</time>
+                      <p class="post-inline-title">Sportland vs Barcelona</p>
+                    </article>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="rd-navbar-main">
-                <div class="rd-navbar-main-top">
-                    <div class="rd-navbar-main-container container">
-                        <!-- RD Navbar Brand-->
-                        <div class="rd-navbar-brand"><a class="brand" href="{{route('Home')}}"><img class="brand-logo "
-                                    src="landing_assets/assets/image/logo_s_b.png" alt="" width="95" height="126"></a>
+            <div class="rd-navbar-panel-item rd-navbar-panel-item-right">
+              <ul class="list-inline list-inline-bordered">
+                <li>
+                  <!-- Select 2-->
+                  <select class="select" name="lang" data-container-class="select-inline" data-dropdown-class="select-inline-dropdown">
+                    <option selected="">fr</option>
+                    
+                    
+                  </select>
+                </li>
+                <li>
+                  <div class="cart-inline-toggled-outer">
+                    <!-- Link Cart-->
+                    <button class="link link-cart cart-inline-toggle" data-rd-navbar-toggle="#cart-inline"><span class="link-cart-icon fa-phone-square"></span></button>
+                    <!-- Cart Inline-->
+                    <article class="cart-inline cart-inline-toggled" id="cart-inline">
+                      <div class="cart-inline-inner">
+                        
+                        
+                        <div class="cart-inline-footer flex flex-row">
+                          <a class="button button-md button-default-outline" >+22968732237</a>
+                          <a class="button button-md button-primary" href="mailto::jaydenfootballacademy@gmail.com">:jaydenfootballacademy@gmail.com</a>
                         </div>
-                        <!-- RD Navbar List-->
-                        <ul class="rd-navbar-list">
-                            <li class="rd-navbar-list-item"><a class="rd-navbar-list-links"><img
-                                        src="landing_assets/assets/image/discipline.png" alt="discipline" width="75"
-                                        height="42"></a></li>
-                            <li class="rd-navbar-list-item"><a class="rd-navbar-list-links"><img
-                                        src="landing_assets/assets/image/passion.png" alt="passion" width="88"
-                                        height="45"></a></li>
-                            <li class="rd-navbar-list-item"><a class="rd-navbar-list-links"><img
-                                        src="landing_assets/assets/image/work-team.png" alt="work-team" width="70"
-                                        height="45"></a></li>
-                        </ul>
-                        <!-- RD Navbar Search-->
-                        <div class="rd-navbar-search">
-                            <ul class="flex flex-row lg:ml-[70px]">
-                                <li class="rd-navbar-list-item"><a class="rd-navbar-list-links"><img
-                                            src="landing_assets/assets/image/soccer-game.png" alt="" width="75"
-                                            height="42"></a></li>
-                                <li class="rd-navbar-list-item"><a class="rd-navbar-list-links"><img
-                                            src="landing_assets/assets/image/football (2).png" alt="" width="88"
-                                            height="45"></a></li>
-                                <li class="rd-navbar-list-item"><a class="rd-navbar-list-links"><img
-                                            src="landing_assets/assets/image/football.png" alt="" width="79"
-                                            height="52"></a></li>
-                            </ul>
-
-
-
-                        </div>
-                    </div>
-                </div>
-                <div class="rd-navbar-main-bottom rd-navbar-darker">
-                    <div class="rd-navbar-main-container container">
-                        <!-- RD Navbar Nav-->
-                        <ul class="rd-navbar-nav">
-                            <li class="rd-nav-item active"><a class="rd-nav-link" href="/">JAYDEN FOOTBALL ACADEMY</a>
-
-                            </li>
-                            <li class="rd-nav-item"><a class="rd-nav-link " href="">Actualité</a></li>
-                            <li class="rd-nav-item"><a class="rd-nav-link" href="{{route('galerie-views')}}">Gallery</a></li>
-
-                            <li class="rd-nav-item active"><a class="rd-nav-link" href="#">Effectif</a>
-                                <ul class="rd-menu rd-navbar-dropdown">
-                                    <li class="rd-dropdown-item"><a class="rd-dropdown-link"
-                                            href="{{route('nosJoueurs-view')}}">Joueurs</a>
+                      </div>
+                    </article>
+                  </div>
+                </li>
+                <li><a class="link link-icon link-icon-left link-classic" href="{{route('login.view')}}"><span class="icon fl-bigmug-line-login12"></span><span class="link-icon-text">Votre Compte</span></a></li>
+              </ul>
+            </div>
+            <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+          </div>
+        </div>
+        <div class="rd-navbar-main">
+          <div class="rd-navbar-main-top">
+            <div class="rd-navbar-main-container container">
+              <!-- RD Navbar Brand-->
+              <div class="rd-navbar-brand"><a class="brand" href="{{route('Home')}}"><img class="brand-logo " src="/landing_assets/assets/image/logo_s_b.png" alt="" width="95" height="126"></a>
+              </div>
+              <!-- RD Navbar List-->
+              <ul class="rd-navbar-list">
+                <li class="rd-navbar-list-item"><a class="rd-navbar-list-links" ><img src="/landing_assets/assets/image/discipline.png" alt="discipline" width="75" height="42"></a></li>
+                <li class="rd-navbar-list-item"><a class="rd-navbar-list-links" ><img src="/landing_assets/assets/image/passion.png" alt="passion" width="88" height="45"></a></li>
+                <li class="rd-navbar-list-item"><a class="rd-navbar-list-links" ><img src="/landing_assets/assets/image/work-team.png" alt="work-team" width="70" height="45"></a></li>
+              </ul>
+              <!-- RD Navbar Search-->
+              <div class="rd-navbar-search">
+                   <ul class="flex flex-row lg:ml-[70px]">
+                      <li class="rd-navbar-list-item"><a class="rd-navbar-list-links" ><img src="/landing_assets/assets/image/soccer-game.png" alt="" width="75" height="42"></a></li>
+                      <li class="rd-navbar-list-item"><a class="rd-navbar-list-links" ><img src="/landing_assets/assets/image/football (2).png" alt="" width="88" height="45"></a></li>
+                      <li class="rd-navbar-list-item"><a class="rd-navbar-list-links" ><img src="/landing_assets/assets/image/football.png" alt="" width="79" height="52"></a></li>
+                    </ul>
+                  
+                 
+                
+              </div>
+            </div>
+          </div>
+          <div class="rd-navbar-main-bottom rd-navbar-darker">
+            <div class="rd-navbar-main-container container">
+              <!-- RD Navbar Nav-->
+                                <ul class="rd-navbar-nav">
+                                    <li class="rd-nav-item active "><a class="rd-nav-link" href="/">JAYDEN FOOTBALL ACADEMY</a>
+                                                       
                                     </li>
-                                    <li class="rd-dropdown-item"><a class="rd-dropdown-link"
-                                            href="{{route('staffs.view')}}">Staff</a>
+                                    <li class="rd-nav-item"><a class="rd-nav-link " href="">Actualité</a>
+                                                                          
                                     </li>
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="{{route('galerie-views')}}">Gallery</a>
+                                                        
+                                    </li>
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="j">Effectif</a>
+                                      <ul class="rd-menu rd-navbar-dropdown">
+                                          <li class="rd-dropdown-item"><a class="rd-dropdown-link"
+                                                  href="{{route('nosJoueurs-view')}}">Joueurs</a>
+                                          </li>
+                                          <li class="rd-dropdown-item"><a class="rd-dropdown-link"
+                                                  href="{{route('staffs.view')}}">Staff</a>
+                                          </li>
+                                      </ul>
+                                  </li>
+
+                                    <li class="rd-nav-item"><a class="rd-nav-link" href="{{route('apropos-view')}}">Qui sommes nous ?</a>
+                                                        
+                                    </li>
+                                    
+                                   
+                                  
                                 </ul>
-                            </li>
-                            </li>
 
-                            <li class="rd-nav-item"><a class="rd-nav-link" href="{{route('apropos-view')}}">Qui sommes nous ?</a></li>
-                        </ul>
-
-                        <ul class="rd-menu rd-navbar-dropdown">
-                            <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="checkout.html">Admission</a>
-                            </li>
-                        </ul>
-                        <div class="">
-                            <li class="rd-nav-item"><a class="rd-nav-link"
-                                    href="{{route('recrutement-view')}}">Recrutement</a>
-
-                            </li>
-
-                        </div>
-                    </div>
-                </div>
+                                
+              <div class="">
+                <li class="rd-nav-item"><a class="rd-nav-link" href="{{route('recrutement-view')}}">Admission</a>
+                  
+                 </li>
+                
+              </div>
             </div>
-        </nav>
+          </div>
+        </div>
+      </nav>
     </div>
-</header>
+  </header>
